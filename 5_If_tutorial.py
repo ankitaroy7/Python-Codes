b674b123d8de91c9ff269e0e0e24e670a2eefa78
Ankita Roy <ankitaroy793@gmail.com>
2024-06-25 09:56:54 +0530

    Add files via upload

---
+++ 5_If_tutorial.py
@@ -0,0 +1,36 @@
+
+
+n = int(input("Enter a number: "))
+#
+# print(type(n))
+#
+# if n % 2 == 0:
+#     print('n is even')
+# else:
+#     print("n is odd")
+
+# Ternary operator:
+message = 'n is even' if n % 2 == 0 else "n is odd"
+print(message)
+
+
+
+################################################################################
+
+# If: conditional flow of code
+
+# indian = ["samosa", "dal", "naan"]
+# chinese = ["egg roll", "pot sticker", "fried rice"]
+# italian = ["pizza", "pasta", "risotto"]
+#
+# dish = input("Enter a dish name: ")
+#
+# if dish in indian:
+#     print(f'{dish} is an indian cuisine')
+# elif dish in chinese:
+#     print(f"{dish} is a chinese cuisine")
+# elif dish in italian:
+#     print(f"{dish} is an italian cuisine")
+# else:
+#     print("I don't know which cuisine is this!")
+
